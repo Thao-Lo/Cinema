1e897c694dbd4351e890162e83b0fea777449292
Zavis <thezavis@gmail.com>
2024-04-26 01:07:24 +1000

    add style for seat page and add selected seats in handle seat click

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var seatScreen = document.querySelector('.seat-container');
 
 var reservationButton = document.getElementById('reserve-button');
 var backButton = document.getElementById('back-button');
+var buyButton = document.getElementById('buy-btn');
 
 reservationButton.addEventListener('click', function () {
     dateScreen.classList.add('d-none');
@@ -83,8 +84,6 @@ var slots = [
         date: "21",
         time: "22:00"
     },
-
-
 ]
 
 
@@ -113,54 +112,42 @@ for (var i = 0; i < slots.length; i++) {
     bookingSelection.appendChild(bookingItem)
 }
 
-// function logCoordinates(event) {
-//     var x;
-//     if (event.touches && event.touches.length > 0) {
-//         // Touch event
-//         x = event.touches[0].clientX;
-//     } else {
-//         // Mouse event
-//         x = event.clientX;
-//     }
-//     console.log(x);
-// }
-
-// // Add event listeners for both touch and mouse events
-// bookingSelection.addEventListener('touchmove', logCoordinates);
-// bookingSelection.addEventListener('mousemove', logCoordinates);
+//Touchable slider - Date Selection
 
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 const firstBookingItem = bookingSelection.querySelector('.booking-item').offsetWidth;
 let isDragging = false;
-var startX, startScrollLeft;
+var startX, startScrollLeft, clientX;
 
 let itemWidth = 80;
 let currentItem;
 var bookingItems = document.querySelectorAll('.booking-item');
 
-prev.addEventListener('touchstart', function () {
+function scrollLeft() {
+    console.log('Scrolling left');
+    console.log('Before scrollLeft:', bookingSelection.scrollLeft);
     bookingSelection.scrollLeft -= (itemWidth);
-})
-next.addEventListener('touchstart', function () {
+    console.log('After scrollLeft:', bookingSelection.scrollLeft);
+
+    clearMargin()
+    currentItem = Math.floor(bookingSelection.scrollLeft / itemWidth)
+    upOne = currentItem + 1;
+    upOneAnother = currentItem + 3;
+    upTwo = currentItem + 2;
+
+    bookingItems[upOne].classList.add('up-one');
+    bookingItems[upOneAnother].classList.add('up-one');
+    bookingItems[upTwo].classList.add('up-two');
+
+}
+function scrollRight() {
+    console.log('Scrolling right');
+    console.log('Before scrollRight:', bookingSelection.scrollLeft);
     bookingSelection.scrollLeft += (itemWidth);
-})
-bookingSelection.addEventListener('touchstart', function (event) {
-    isDragging = true;
-    bookingSelection.classList.add('dragging');
+    console.log('After scrollRight:', bookingSelection.scrollLeft);
 
-    //record the initial touch/cursor on the screen
-    startX = event.touches[0].clientX;
-    console.log('StartX :' + startX);
-
-    //record the amount of pixel the scroll the the left and out of the container
-    startScrollLeft = bookingSelection.scrollLeft;
-    console.log('startScrollLeft :' + startScrollLeft);
-})
-function dragMove(event) {
-    //Update the scroll position of the slider based on the cursor movement
-    bookingSelection.scrollLeft = startScrollLeft - (event.touches[0].clientX - startX);
-    console.log('bookingSelection.scrollLeft' + bookingSelection.scrollLeft + " =" + 'startScrollLeft ' + startScrollLeft + 'event.touches[0].clientX : ' + event.touches[0].clientX + 'startX' + startX);
+    clearMargin()
     currentItem = Math.floor(bookingSelection.scrollLeft / itemWidth)
     upOne = currentItem + 1;
     upOneAnother = currentItem + 3;
@@ -170,84 +157,78 @@ function dragMove(event) {
     bookingItems[upOneAnother].classList.add('up-one');
     bookingItems[upTwo].classList.add('up-two');
 }
+
+prev.addEventListener('touchstart', scrollLeft)
+prev.addEventListener('click', scrollLeft)
+
+next.addEventListener('touchstart', scrollRight)
+next.addEventListener('click', scrollRight)
+
+
+
+const dragStart = (event) => {
+    isDragging = true;
+    bookingSelection.classList.add('dragging');
+    //record the initial touch/cursor on the screen
+    if (event.touches && event.touches.length > 0) {
+        startX = event.touches[0].clientX;
+    } else {
+        startX = event.clientX;
+    }
+    console.log('StartX :' + startX);
+
+    //record the amount of pixel the scroll the the left and out of the container
+    startScrollLeft = bookingSelection.scrollLeft;
+    console.log('startScrollLeft :' + startScrollLeft);
+}
+
 function clearMargin() {
     for (var i = 0; i < bookingItems.length; i++) {
         bookingItems[i].classList.remove('up-one', 'up-two');
     }
 }
-
-bookingSelection.addEventListener('touchmove', function (event) {
-    clearMargin()
+const dragMove = (event) => {
     if (!isDragging) return
-    dragMove(event);
+    //Update the scroll position of the slider based on the cursor movement
 
-});
+    if (event.touches && event.touches.length > 0) {
+        clientX = event.touches[0].clientX;
+    } else {
+        clientX = event.clientX;
+    }
+    bookingSelection.scrollLeft = startScrollLeft - (clientX - startX);
+    updateMargin()
+}
 
-document.addEventListener('touchend', function () {
+
+const updateMargin = () => {
+    clearMargin()
+    currentItem = Math.floor(bookingSelection.scrollLeft / itemWidth)
+    upOne = currentItem + 1;
+    upOneAnother = currentItem + 3;
+    upTwo = currentItem + 2;
+
+    bookingItems[upOne].classList.add('up-one');
+    bookingItems[upOneAnother].classList.add('up-one');
+    bookingItems[upTwo].classList.add('up-two');
+}
+const dragEnd = () => {
     isDragging = false;
     bookingSelection.classList.remove('dragging');
-})
+}
+//mobile
+bookingSelection.addEventListener('touchstart', dragStart)
+bookingSelection.addEventListener('touchmove', dragMove);
+document.addEventListener('touchend', dragEnd)
+
+//window
+bookingSelection.addEventListener('mousedown', dragStart)
+bookingSelection.addEventListener('mousemove', dragMove);
+document.addEventListener('mouseup', dragEnd)
 
 
 // TEMPLATE STRING
-//SEAT
-// var seatRowOneHTML = document.querySelector("#seat-row-one");
-// var seatRowTwoHTML = document.querySelector("#seat-row-two");
-// var seatRowThreeHTML = document.querySelector("#seat-row-three");
-// var seatRowFourHTML = document.querySelector("#seat-row-four");
-// var seatRowFiveHTML = document.querySelector("#seat-row-five");
-// var seatRowSixHTML = document.querySelector("#seat-row-six");
-
-// var seatRowsHTML = [seatRowOneHTML, seatRowTwoHTML, seatRowThreeHTML, seatRowFourHTML, seatRowFiveHTML, seatRowSixHTML]
-// var seatRows = [seat_row_one, seat_row_two, seat_row_three, seat_row_four, seat_row_five, seat_row_six]
-
-// for (var j = 0; j < seatRows.length; j++) {
-//     var currentRow = seatRows[j];
-//     var currentRowHTML = seatRowsHTML[j];
-//     console.log(currentRow);
-//     if (j <= 1) {
-//         var firstHalfHTML = currentRowHTML.querySelector(".seat-row:first-child");
-//         var secondHalfHTML = currentRowHTML.querySelector(".seat-row:last-child");
-
-//         var midpoint = Math.ceil(seatRows[j].length / 2);
-//         for (var k = 0; k < midpoint; k++) {
-//             var seat = generateSeat(currentRow[k]);
-//             console.log("hello" + k);
-//             firstHalfHTML.appendChild(seat);
-//         }
-//         for (var k = midpoint; k < currentRow.length; k++) {
-//             var seat = generateSeat(currentRow[k]);
-//             console.log("hello 2 = " + k);
-//             secondHalfHTML.appendChild(seat);
-//         }
-//     } else {
-//         for (var i = 0; i < seatRows[j].length; i++) {
-//             var seat = generateSeat(currentRow[i]);
-//             seatRowsHTML[j].appendChild(seat);
-//         }
-//     }
-// }
-// function generateSeat(currentSeat) {
-//     var seat = document.createElement('div');
-//     seat.classList.add('seat');
-//     if (!currentSeat.available) {
-//         seat.classList.add('unavailable')
-//     }
-//     seat.addEventListener('click', handleSeatClick);
-//     seat.innerHTML = ` <svg width="30" height="22" viewBox="0 0 30 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//         d="M0 4C0 2.89543 0.89543 2 2 2H4C5.10457 2 6 2.89543 6 4V14C6 15.1046 6.89543 16 8 16H22C23.1046 16 24 15.1046 24 14V4C24 2.89543 24.8954 2 26 2H28C29.1046 2 30 2.89543 30 4V17C30 19.7614 27.7614 22 25 22H5C2.23858 22 0 19.7614 0 17V4Z"
-//         fill="#D9D9D9" />
-//     <path
-//         d="M7 3C7 1.34315 8.34315 0 10 0H20C21.6569 0 23 1.34315 23 3V14C23 14.5523 22.5523 15 22 15H8C7.44772 15 7 14.5523 7 14V3Z"
-//         fill="#D9D9D9" />
-//     </svg>`
-//     return seat;
-// }
-// function handleSeatClick(event) {
-//     event.target.classList.toggle('selected');
-// }
-
+// ------ SEAT ADD & REMOVE
 var seatRowOneLeftHTML = document.querySelector('#seat-row-one>.seat-row:first-child')
 var seatRowOneRightHTML = document.querySelector('#seat-row-one>.seat-row:last-child')
 
@@ -259,19 +240,48 @@ var seatRowFourHTML = document.querySelector("#seat-row-four");
 var seatRowFiveHTML = document.querySelector("#seat-row-five");
 var seatRowSixHTML = document.querySelector("#seat-row-six");
 
+var seatSelectedNameHTML = document.getElementById('selected-seats-name');
+var totalPriceHTML = document.getElementById('total-price');
 
-
+var selectedSeats = []
 var totalPrice = 0;
+
+function generateSeatNameAndPrice(){
+    //clear inner HTML to generate the seats again when loop through the array to avoid duplicate 'seat1/ seat 1 seat 3/ seat1 seat3 seat4'
+    seatSelectedNameHTML.innerHTML = '';
+    totalPriceHTML.innerHTML ='';
+
+    for(var i = 0; i <selectedSeats.length; i++){
+        var currentSeat = selectedSeats[i];
+        seatSelectedNameHTML.innerHTML = seatSelectedNameHTML.innerHTML + currentSeat.id;
+        totalPriceHTML.innerHTML = +totalPriceHTML.innerHTML + +currentSeat.price;
+    }
+}
 
 function handleSeatClick(event) {
     var available = event.target.dataset.available;
+    var id = event.target.dataset.id;
+    var price = event.target.dataset.price;
+    var isVip = event.target.dataset.isVip;
 
-    if (available === 'true') {
-        event.target.classList.toggle('selected')
+    console.log(available);
+    if (available === 'true') {        
+        if(event.target.classList.value.includes('selected')){
+            event.target.classList.remove('selected');
+        }else{
+            event.target.classList.add('selected');
+            var newSeat = {
+                id: id,
+                price: price,
+                isVip: isVip
+            }
+            selectedSeats.push(newSeat);
+            console.log(selectedSeats);
+            generateSeatNameAndPrice();
+        }
     }
-
-
 }
+
 function generateSeats(rowData, seatRowHTML) {
     for (var i = 0; i < rowData.length; i++) {
         var currentSeat = rowData[i]
@@ -280,8 +290,8 @@ function generateSeats(rowData, seatRowHTML) {
         seat.classList.add('seat');
         seat.dataset.available = currentSeat.available;
         seat.dataset.id = currentSeat.id;
-        seat.dataset.isVip = currentSeat.isVip;
         seat.dataset.price = currentSeat.price;
+        seat.dataset.isVip = currentSeat.isVip;
 
         if (!currentSeat.available) {
             seat.classList.add('unavailable');
@@ -302,13 +312,14 @@ function generateSeats(rowData, seatRowHTML) {
     }
 }
 
-generateSeats(seat_row_one.slice(0, seat_row_one.length / 2), seatRowOneLeftHTML);
-generateSeats(seat_row_one.slice(seat_row_one.length / 2), seatRowOneRightHTML);
+generateSeats(seat_row_one.slice(0, seat_row_one.length / 2), seatRowOneLeftHTML)
+generateSeats(seat_row_one.slice(seat_row_one.length / 2), seatRowOneRightHTML)
 
-generateSeats(seat_row_two.slice(0, seat_row_two.length / 2), seatRowTwoLeftHTML);
-generateSeats(seat_row_two.slice(seat_row_two.length / 2), seatRowTwoRightHTML);
+generateSeats(seat_row_two.slice(0, seat_row_two.length / 2), seatRowTwoLeftHTML)
+generateSeats(seat_row_two.slice(seat_row_two.length / 2), seatRowTwoRightHTML)
 
 generateSeats(seat_row_three, seatRowThreeHTML)
 generateSeats(seat_row_four, seatRowFourHTML)
 generateSeats(seat_row_five, seatRowFiveHTML)
 generateSeats(seat_row_six, seatRowSixHTML)
+
